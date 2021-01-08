b085031813b39e0005e280d168ff113bf76f6c36
Margaux Vitez <margaux.vitez@hotmail.fr>
2021-01-08 15:06:14 +0100

    [MV] add scroll to top

--- js/theme.js
+++ js/theme.js
@@ -222,7 +222,28 @@
     + "m" + second + "s");
 
     $('#modal_date').html('<p> Nous sommes le ' + daylist[day] + ' ' + day + ' ' + monthlist[month] + ', et il est ' + hour + "h" + minute + "m" + second + "s </p>");
-    
+
+    /*----------------------------------------------------*/
+    /*  Scroll to top button 
+    /*----------------------------------------------------*/
+    $(document).ready(function(){
+        $(window).scroll(function () {
+                if ($(this).scrollTop() > 50) {
+                    $('#back-to-top').fadeIn();
+                } else {
+                    $('#back-to-top').fadeOut();
+                }
+            });
+            // scroll body to 0px on click
+            $('#back-to-top').click(function () {
+                $('body,html').animate({
+                    scrollTop: 0
+                }, 400);
+                return false;
+            });
+    });
+
+
 	/*----------------------------------------------------*/
     /*  Google map js
     /*----------------------------------------------------*/
